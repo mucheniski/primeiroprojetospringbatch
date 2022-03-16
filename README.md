60560863e2ff8a394d92cb4cb343ffdbdf702e64
Diego Mucheniski <mucheniski@gmail.com>
2022-03-16 19:53:51 -0300

    13. Prática: Múltiplas execuções e escopo

--- README.md
+++ README.md
@@ -7,6 +7,8 @@ através dele conseguimos várias informações sobre a execução dos jobs.
 BATCH_JOB_INSTANCE - Todas as execuções com sucesso.  
 BATCH_JOB_EXECUTION - Todas as execuções independente de sucesso ou não, mostra o status.  
 BATCH_JOB_EXECUTION_CONTEXT - Informações adicionais que podem ser passadas no contexto de execução, como informações de negócio por exemplo.  
+BATCH_JOB_EXECUTION_PARAMS - Apresenta os parametros de execução do job.  
+
 BATCH_STEP_EXECUTION - Verificar quais steps executaram.  
 BATCH_STEP_EXECUTION_CONTEXT - Informações do contexto do step.  
 
--- src/main/java/com/example/primeiroprojetospringbatch/config/BatchConfig.java
+++ src/main/java/com/example/primeiroprojetospringbatch/config/BatchConfig.java
@@ -6,10 +6,13 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,22 +31,29 @@ public class BatchConfig {
         return jobBuilderFactory
                 .get("imprimeOlaJob")
                 .start(imprimeOlaStep())
+                .incrementer(new RunIdIncrementer()) // Serve para podermos executar o mesmo job mais de uma vez, incrementando o id para diferenciar
                 .build();
     }
 
     public Step imprimeOlaStep() {
         return stepBuilderFactory
                 .get("imprimeOlaStep")
-                .tasklet(stepTaskletImprimeOla)
+                .tasklet(stepTaskletImprimeOla(null))
                 .build();
     }
 
-    private Tasklet stepTaskletImprimeOla = new Tasklet() {
-        @Override
-        public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-            System.out.println("Olá, mundo!");
-            return RepeatStatus.FINISHED;
-        }
-    };
+    // "#{jobParameters['nome']}" obtem o nome do parametro em tempo de execução e passa para a tasklet
+    // O método precisa estar no contexto de escopo de step com as anotações abaixo, e precisa ser public para o spring ter acesso
+    @Bean
+    @StepScope
+    public Tasklet stepTaskletImprimeOla(@Value("#{jobParameters['nome']}") String nome) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Olá, %s!", nome));
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
 
 }
