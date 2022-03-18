e9a133eeb537878a8b76553dc1476b1e8b9a595b
Diego Mucheniski <mucheniski@gmail.com>
2022-03-18 17:35:17 -0300

    30. Prática: Leitura de arquivo delimitado

--- src/main/java/com/example/primeiroprojetospringbatch/step/ArquivoLarguraFixaStepConfig.java
+++ src/main/java/com/example/primeiroprojetospringbatch/step/ArquivoLarguraFixaStepConfig.java
@@ -1,28 +1,28 @@
-package com.example.primeiroprojetospringbatch.step;
-
-import com.example.primeiroprojetospringbatch.domain.Cliente;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ArquivoLarguraFixaStepConfig {
-
-    @Autowired
-    public StepBuilderFactory stepBuilderFactory;
-
-    @Bean
-    public Step ArquivoLarguraFixaStep(ItemReader<Cliente> arquivoLarguraFixaReader, ItemWriter<Cliente> arquivoLarguraFixaWriter) {
-        return stepBuilderFactory
-                .get("ArquivoLarguraFixaStep")
-                .<Cliente, Cliente>chunk(1) // Le Cliente e escreve Cliente
-                .reader(arquivoLarguraFixaReader)
-                .writer(arquivoLarguraFixaWriter)
-                .build();
-    }
-
-}
+//package com.example.primeiroprojetospringbatch.step;
+//
+//import com.example.primeiroprojetospringbatch.domain.Cliente;
+//import org.springframework.batch.core.Step;
+//import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+//import org.springframework.batch.item.ItemReader;
+//import org.springframework.batch.item.ItemWriter;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ArquivoLarguraFixaStepConfig {
+//
+//    @Autowired
+//    public StepBuilderFactory stepBuilderFactory;
+//
+//    @Bean
+//    public Step arquivoLarguraFixaStep(ItemReader<Cliente> arquivoLarguraFixaReader, ItemWriter<Cliente> arquivoLarguraFixaWriter) {
+//        return stepBuilderFactory
+//                .get("arquivoLarguraFixaStep")
+//                .<Cliente, Cliente>chunk(1) // Le Cliente e escreve Cliente
+//                .reader(arquivoLarguraFixaReader)
+//                .writer(arquivoLarguraFixaWriter)
+//                .build();
+//    }
+//
+//}
