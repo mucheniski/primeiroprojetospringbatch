170880ac61740da0b5e1664548f209148a77cf3e
mucheniski <mucheniski@gmail.com>
2022-03-23 18:01:31 -0300

    jdbc cursor reader

---
+++ src/main/java/com/example/primeiroprojetospringbatch/readers/jdbccursorreader/writer/JdbcCursorWriterConfig.java
@@ -0,0 +1,15 @@
+package com.example.primeiroprojetospringbatch.readers.jdbccursorreader.writer;
+
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springbatch.jdbccursorreader.dominio.Cliente;
+
+@Configuration
+public class JdbcCursorWriterConfig {
+	@Bean
+	public ItemWriter<Cliente> jdbcCursorWriter() {
+		return clientes -> clientes.forEach(System.out::println);
+	}
+}
